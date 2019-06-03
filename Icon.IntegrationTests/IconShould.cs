c18c5e6234ae82dd522366760f9b687e127286ea
Hilary Sasso-Schleh <hilary.sasso-schleh@accenture.com>
2019-06-03 11:24:01 -0400

    added geckdriver

--- Icon.IntegrationTests/IconShould.cs
+++ Icon.IntegrationTests/IconShould.cs
@@ -1,13 +1,29 @@
 using System;
 using Xunit;
+using Shouldly;
+using OpenQA.Selenium.Firefox;
+
 
 namespace Icon.IntegrationTests
 {
     public class IconShould
     {
         [Fact]
-        public void Test1()
+        public void DisplayExpectedText()
         {
+            var currentDirectory = Environment.CurrentDirectory;
+            var services = FirefoxDriverService.CreateDefaultService(currentDirectory, 
+                "geckodrivermac");
+            var driver = new FirefoxDriver(services);
+            
+            driver.Navigate().GoToUrl("http://localhost:5000/");
+
+            var result = driver.FindElementByTagName("pre").Text;
+            
+            driver.Quit();
+            
+            result.ShouldContain("car");
+            
         }
     }
 }
