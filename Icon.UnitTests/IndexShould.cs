5be1a7d04d5fe6fea6133cc8c1b229cda8660025
Hilary Sasso-Schleh <hilary.sasso-schleh@accenture.com>
2019-06-04 15:38:05 -0400

    added unit test directory and secret in csproj

---
+++ Icon.UnitTests/IndexShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using Shouldly;
+
+namespace Icon.UnitTests
+{
+    public class IndexShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
