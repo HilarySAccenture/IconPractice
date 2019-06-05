3a09acdf9c6b5320e945b3afc9e17c7801425d53
Hilary Sasso-Schleh <hilary.sasso-schleh@accenture.com>
2019-06-05 13:47:33 -0400

    mocking restclient and restresponse

--- Icon.UnitTests/ApiCallerShould.cs
+++ Icon.UnitTests/ApiCallerShould.cs
@@ -1,18 +1,30 @@
+using System;
 using IconPractice.Domain;
 using Xunit;
 using Shouldly;
 using NSubstitute;
+using RestSharp;
 
 namespace Icon.UnitTests
 {
     public class ApiCallerShould
     {
         [Fact]
-        public void Exist()
+        public void ReturnStringWhenGetArticlesAsJsonCalled()
         {
-            var caller = new ApiCaller();
+            
+            var _client = Substitute.For<IRestClient>();
+            var _response = Substitute.For<IRestResponse>();
+            _response.Content.Returns("spartacus");
+            _client.Execute(Arg.Any<IRestRequest>()).Returns(_response);
+            string apiKey = null;
+            
+            var caller = new ApiCaller(_client, apiKey);
 
-            caller.ShouldNotBeNull();
+            var response = caller.GetArticlesAsJson();
+            
+            response.ShouldNotBeNullOrEmpty();
+
         }
     }
 }
--- IconPractice/Domain/ApiCaller.cs
+++ IconPractice/Domain/ApiCaller.cs
@@ -1,10 +1,40 @@
+using System;
+using RestSharp;
+
 namespace IconPractice.Domain
 {
     public class ApiCaller : IApiCaller
     {
+        private IRestClient _restClient;
+        private string _apiKey;
+
+        public ApiCaller(IRestClient restClient, string apiKey)
+        {
+            _restClient = restClient;
+            _apiKey = apiKey;
+        }
+
+        public ApiCaller()
+        {
+            _restClient = new RestClient("https://api.currentsapi.services/v1/");
+            _apiKey = Environment.GetEnvironmentVariable("CURRENT_API_KEY");
+        }
+        
         public string GetArticlesAsJson()
         {
-            throw new System.NotImplementedException();
+            var request = CreateRestRequest();
+
+            var response = _restClient.Execute(request);
+
+            return response.Content;
+        }
+
+        private RestRequest CreateRestRequest()
+        {
+            var request = new RestRequest("latest-news", Method.GET);
+            request.AddHeader("Authorization", _apiKey);
+
+            return request;
         }
     }
 }
