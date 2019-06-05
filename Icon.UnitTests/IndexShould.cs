a756e51468b7c9110bbf0716cf41f3457539377c
Hilary Sasso-Schleh <hilary.sasso-schleh@accenture.com>
2019-06-05 11:50:23 -0400

    first unit test index displays text

--- Icon.UnitTests/IndexShould.cs
+++ Icon.UnitTests/IndexShould.cs
@@ -1,14 +1,38 @@
 using System;
+using OpenQA.Selenium.Firefox;
 using Xunit;
 using Shouldly;
+using OpenQA.Selenium;
 
 namespace Icon.UnitTests
 {
     public class IndexShould
     {
         [Fact]
-        public void Test1()
+        public void DisplayExpectedText()
         {
+            var currentDirectory = Environment.CurrentDirectory;
+            var fileName = "geckodrivermac";
+            var services = FirefoxDriverService.CreateDefaultService(currentDirectory, fileName);
+            var driver = new FirefoxDriver(services);
+
+            var result = string.Empty;
+            
+            try
+            {
+                driver.Navigate().GoToUrl("http://localhost:5000/");
+                result = driver.FindElementByTagName("p").Text;
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                driver.Quit();
+            }
+            result.ShouldContain("gattaca");
         }
     }
 }
